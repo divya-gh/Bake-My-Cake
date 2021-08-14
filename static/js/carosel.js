af2a8afb6fb6fc51996789a22342e067a4a23212
Divya Shetty <divyashettyk@gmail.com>
2021-08-14 15:19:00 -0500

    Add carrousel

--- static/js/carosel.js
+++ static/js/carosel.js
@@ -1,38 +1,19 @@
-$(document).ready(function(){
+var slideIndex = 0;
+showSlides();
 
-    $('.items').slick({
-    dots: true,
-    infinite: true,
-    speed: 800,
-    autoplay: true,
-    autoplaySpeed: 2000,
-    slidesToShow: 4,
-    slidesToScroll: 4,
-    responsive: [
-    {
-    breakpoint: 1024,
-    settings: {
-    slidesToShow: 3,
-    slidesToScroll: 3,
-    infinite: true,
-    dots: true
-    }
-    },
-    {
-    breakpoint: 600,
-    settings: {
-    slidesToShow: 2,
-    slidesToScroll: 2
-    }
-    },
-    {
-    breakpoint: 480,
-    settings: {
-    slidesToShow: 1,
-    slidesToScroll: 1
-    }
-    }
-    
-    ]
-    });
-    });
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
